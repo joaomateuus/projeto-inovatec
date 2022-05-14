1c604cb4266eb2df92492cc24fbaf8c6d8015a27
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-14 17:21:03 -0300

    commiting commit

--- backend/nutricional/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Nutricional
-
-class NutricionalSerializer(ModelSerializer):
-    class Meta:
-        model = Nutricional
-        fields = '__all__'
