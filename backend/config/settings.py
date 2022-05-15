f921731560d50546b853075cef34e818e970d584
zeguil <zeguilhermelins@hotmail.com>
2022-05-15 16:52:28 -0400

    cors headers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,14 +30,23 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'django_filters',
+    'corsheaders',
     #apps
     'cardapio',
     'nutricional',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    
+    #! porta onde o frontend vai rodar
+    'http://localhost:8080',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    #! cors headers
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
