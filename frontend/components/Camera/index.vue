fe264c20bdeb27c4ce645bbd1791debd241b9cfb
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-20 15:00:20 -0300

    commiting

--- frontend/components/Camera/index.vue
+++ frontend/components/Camera/index.vue
@@ -8,15 +8,16 @@
 export default {
     name: 'Camera',
     methods: {
-         startCamera() {
+        startCamera() {
             navigator.mediaDevices
-            .getUserMedia({video:true, width:1000}).then(stream => {
+            .getUserMedia({video:true, width:1000})
+            .then(stream => {
                 const videoElement = document.querySelector('#video');
                 videoElement.srcObject = stream;
             }).catch(error => {
             alert("Browser doesn't support or there is some errors." + error);
         });
-    }
+        }
     },
     mounted() {
         this.startCamera();
