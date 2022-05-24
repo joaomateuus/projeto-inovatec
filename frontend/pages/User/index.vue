97d5de3c9b522ef6092f81537a08411b6bc0a8be
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-24 18:16:56 -0300

    page redirect

--- frontend/pages/User/index.vue
+++ frontend/pages/User/index.vue
@@ -10,6 +10,12 @@
           <v-card class="mx-auto" min-width="400" height="500" tile>
             <v-list flat>
               <v-list-item-group v-model="selectedItem" color="primary">
+                <a href="https://sites.google.com/view/modernmenu/p%C3%A1gina-inicial">
+                  <v-list-item class="flex">
+                    <v-icon>mdi-alert-circle</v-icon>
+                    <v-list-item-title class="ml-4">Sobre</v-list-item-title>
+                  </v-list-item>
+                </a>
                 <v-list-item v-for="(item, i) in items" :key="i">
                   <v-list-item-icon>
                     <v-icon v-text="item.icon"></v-icon>
@@ -35,7 +41,6 @@ export default {
         selectedItem: 1,
         items: [
           { text: 'Menu', icon: 'mdi-food', route:'/' },
-          { text: 'Sobre', icon: 'mdi-alert-circle', route:'/sobre' },
           { text: 'Avaliar', icon: 'mdi-star', route:'/' },
         ],
     }),
