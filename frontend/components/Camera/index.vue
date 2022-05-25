aa3bb7e09a58b9fb0c3b1de2d0bdb6cbe12db277
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-25 11:49:31 -0300

    getting frontcamera

--- frontend/components/Camera/index.vue
+++ frontend/components/Camera/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-fit max-w-sm rounded-md">
-        <video id="video" height="100" width="350" autoplay class="rounded-lg" />
+        <video id="video" autoplay class="rounded-lg" />
         <div class="flex items-center justify-between p-4 bg-white rounded-md">
             <button @click="startCamera()" class="mt-4 ml-6 mb-4 h-12 w-32 text-lg text-white bg-green-600 rounded-full">
                 <v-icon>mdi-camera</v-icon>
@@ -15,6 +15,25 @@
 <script>
 export default {
     name: 'Camera',
+    data: () => ({
+        constraints: {
+            video: {
+                height: {
+                    min: 1280,
+                    ideal: 1920,
+                    max: 2560
+                },
+                width: {
+                    min: 720,
+                    ideal: 1080,
+                    max: 1440
+                },
+                facingMode: {
+                    exact: 'environment'
+                }
+            }
+        }
+    }),
     methods: {
         startCamera() {
             navigator.mediaDevices
