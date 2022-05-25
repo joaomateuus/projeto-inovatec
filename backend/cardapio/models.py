96b690b7c0a3ac3bdcb28f989ac7083a7e94d7b5
zeguil <zeguilhermelins@hotmail.com>
2022-05-25 18:46:37 -0400

    att

--- backend/cardapio/models.py
+++ backend/cardapio/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+def upload_image_book(instance, filename):
+    return f"{instance.id_book}-{filename}"
 
 class Prato(models.Model):
     nome = models.CharField(max_length=40)
     valor = models.FloatField()
     detalhes = models.TextField()
-    foto = models.ImageField(upload_to='pratos/', null=True, blank=True)
+    foto = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
 
     def __str__(self):
         return self.nome
